92878e6d912363b60a9e50fb6b9cf0f3a63334ca
nizhantiw <nizhantiw@gmail.com>
2022-11-22 20:00:43 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+     constructor() {
+          this.color = "green";
+     }
+     drawShape() {
+
+     }
+     calculateArea() {
+
+     }
+}
+// export class using module.exports
+module.exports = Shape;
